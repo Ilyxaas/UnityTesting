deee90c6b84c07ff131fd8952701657fe1ff6cf8
Ilyaxaas <esin54321@yandex.ru>
2023-06-19 20:47:50 +0300

    first

---
+++ Assets/Scripts/Flower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    public static Flower instance { get; private set; }
+    // Start is called before the first frame update
+
+    private Flower()
+    {
+    
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
